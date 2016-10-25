1765b78dfea535e9192d2ebfffaf46f7063707b1
Augusto Pedraza <augusto.pedraza08@gmail.com>
2016-10-25 08:06:28 -0300

    updated gitignore and fixed module error

---
+++ ch02/ex1-shop/drop.ex
@@ -0,0 +1,12 @@
+defmodule Drop do
+  def fall_velocity(distance) do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  def mps_to_mph(mps) do
+    2.23693629 * mps
+  end
+
+  def mps_to_kph(mph), do: 3.6 * mph
+end
+
